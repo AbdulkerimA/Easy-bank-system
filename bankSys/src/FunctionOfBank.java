b0e1c8670345d4ccb28f6a479cd5b0ed0e521f5d
abdulkerim <114300499+AbdulkerimA@users.noreply.github.com>
2022-11-16 23:24:18 -0800

    an update on the src file

---
+++ bankSys/src/FunctionOfBank.java
@@ -0,0 +1,5 @@
+public class FunctionOfBank {
+    private String userName;
+    private double deposit,withdraw,transfer;
+    
+}
--- bankSys/src/Main.java
+++ bankSys/src/Main.java
@@ -1,22 +1,58 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        int option = 0;// interface option
+        Byte option = 0;// interface option
         String name; // name for account creator
-        double dipAmount = 0 ; // deposit amount
+        double dipAmount = 0  ; // deposit amount
         double withAmount = 0 ; // Withdrawal amount
         Scanner input = new Scanner(System.in);
 
         while (option != 5 ){
             display();
-            option = input.nextInt();
+            option = input.nextByte();
+
+            // checking if the user pressed the wrong button
+
+            if (option > 5 || option < 1){
+                System.out.println("you pressed the wrong option pleas try again");
+                display();
+                option = input.nextByte();
+            }
+            // if the user press one of the given option correctly the following code will be run
+            else {
+                // so we will check the user option hear and per form the required action
+                switch (option){
+                    case 1:
+                        // taking username and amount of deposit
+
+                        System.out.println("Enter the user full name : ");
+                        name = input.nextLine();
+                        System.out.println("Enter Deposit amount : ");
+                        dipAmount = input.nextDouble();
+
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                }
+            }
         }
 
     }
 
-    // almost all of the things that are displayed on the system will came from this function
+    // almost all things that are displayed on the system will came from this function
     public static void display (){
         System.out.println("\t\t\t +======================+");
         System.out.println(" \t\t\t| Welcome to JAVA Bank |");
