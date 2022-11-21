c223b11ae10b4748cc01c4877c75f76ed6f31455
abdulkerim <114300499+AbdulkerimA@users.noreply.github.com>
2022-11-21 05:35:18 -0800

    new class and function it is also working

--- bankSys/src/FunctionOfBank.java
+++ bankSys/src/FunctionOfBank.java
@@ -1,5 +1,104 @@
+import java.util.ArrayList;
+
 public class FunctionOfBank {
-    private String userName;
-    private double deposit,withdraw,transfer;
-    
-}
+    private ArrayList<String> userName = new ArrayList<>();
+    private ArrayList<Double> deposit = new ArrayList<Double>();
+    //private ArrayList<Double> withdraw = new ArrayList<>();
+    //private ArrayList<Double> transfer = new ArrayList<>();
+
+    public void register(String userName, double deposit) {
+        this.userName.add(userName);
+        this.deposit.add(deposit);
+    }
+
+    /*
+     * what this function will do is that if the existing user wants to deposit some amount of money
+     * it will acept that money and add to the account deposit
+     * */
+    public void setDeposit (String name , double depAmount) {
+        // we need to search for the username
+        String ownerName ;
+        for (int i=0 ; i < this.userName.size();i++){
+
+            ownerName = this.userName.get(i);
+
+            if (ownerName.equals(name)){
+                // clear the screen
+                System.out.println("The deposit amount is : " + depAmount + " ETB ");
+
+                this.deposit.set( i , ( this.deposit.get(i) + depAmount) );
+
+                System.out.println("Your current balance is  : " + this.deposit.get(i) + " ETB");
+
+                // press something to continue
+            }
+            else {
+
+            }
+        }// end of loop
+    }// end of func
+
+
+    // what this fuc will do is when someone withdraw some amount of money from their account
+    // it will check the right owner of the account and decreas their deposit amount
+
+    public void setWithdraw (String name , double withAmount){
+
+        // we need to search for the username first
+
+        String ownerName ;
+        for (int i=0 ; i < this.userName.size();i++){
+
+            ownerName = this.userName.get(i);
+
+            if (ownerName.equals(name)){
+                // clear the screen
+                System.out.println("dear customer the withdrawn amount is : " + withAmount + " ETB");
+
+                this.deposit.set( i , ( this.deposit.get(i) - withAmount) );
+
+                System.out.println("Your current balance is : " + this.deposit.get(i) + " ETB");
+
+                // press something to continue
+            }
+            else {
+
+            }
+        }// the end of for loop
+    }// the end of the function
+
+        /*
+            what this function will do is it allow users to transfer from one account to another
+            using the  name  of the user they want to transfer money to
+            It takes two usernames sender and resiver and it will take the amount to transer
+            then it will decreas the deposit amount from sender and add it to resiver deposit
+        */
+    public void setTransfer (String sender , String resiver , double transferAmount) {
+
+        // checking the user have sufficent amount of money
+        // identifaing who is the sender
+
+        for (int i=0; i<this.userName.size() ; i++ ) {
+
+            if (sender.equals( this.userName.get(i) ) ) {
+
+                    this.deposit.set( i , ( this.deposit.get(i) - transferAmount) );
+
+                System.out.println("\t\t\t Transfer successful TO " + resiver );
+                System.out.println("dear customer your current balance is " + this.deposit.get(i) + " ETB");
+
+            } // the end of the if statment
+
+            // identifing the resiver and adding the transfer amount
+
+            if (resiver.equals( this.userName.get(i) ) ) {
+
+                this.deposit.set( i , ( this.deposit.get(i) + transferAmount) );
+
+            }// the end of if stetment
+
+        }// end of for loop
+    }
+}// the end of the class
+
+
--- bankSys/src/Main.java
+++ bankSys/src/Main.java
@@ -7,7 +7,9 @@ public class Main {
         String name; // name for account creator
         double dipAmount = 0  ; // deposit amount
         double withAmount = 0 ; // Withdrawal amount
+        double transAmount = 0; // transfer amount
         Scanner input = new Scanner(System.in);
+        FunctionOfBank javaBank = new FunctionOfBank();
 
         while (option != 5 ){
             display();
@@ -20,7 +22,7 @@ public class Main {
                 display();
                 option = input.nextByte();
             }
-            // if the user press one of the given option correctly the following code will be run
+            // if the user press one of the given option correctly then the following code will be run
             else {
                 // so we will check the user option hear and per form the required action
                 switch (option){
@@ -28,23 +30,47 @@ public class Main {
                         // taking username and amount of deposit
 
                         System.out.println("Enter the user full name : ");
-                        name = input.nextLine();
+                        name = input.next();
                         System.out.println("Enter Deposit amount : ");
                         dipAmount = input.nextDouble();
-
-
+                        javaBank.register(name,dipAmount);
                         break;
                     case 2:
+                        System.out.printf("Enter the username to deposit on : ");
+                        name = input.next();
+
+                        System.out.println("Enter the amount of money to deposit : ");
+                        dipAmount = input.nextDouble();
+
+                        javaBank.setDeposit(name , dipAmount);
 
                         break;
                     case 3:
+                        System.out.printf("pleas Enter the account owner name : ");
+                        name = input.next();
+                        System.out.printf("How much money do you want to withdraw : ");
+                        withAmount = input.nextDouble();
+
+                        javaBank.setWithdraw(name,withAmount);
 
                         break;
                     case 4:
+                        String senderName ,receiverName ; // sender name and resiver name
+
+                        System.out.printf("pleas Enter the Sender name : ");
+                        senderName = input.next();
+
+                        System.out.printf("pleas Enter the receiver name : ");
+                        receiverName = input.next();
+
+                        System.out.printf("pleas Enter the Amount of money to transfer : ");
+                        transAmount = input.nextDouble();
+
+                        javaBank.setTransfer(senderName , receiverName , transAmount);
 
                         break;
                     case 5:
-
+                        // this will automatically end the process
                         break;
                 }
             }
@@ -59,7 +85,7 @@ public class Main {
         System.out.println("\t\t\t +======================+");
 
         System.out.println("* To create an account press 1 ");
-        System.out.println("* To Deposit to an existing account press 2 ");
+        System.out.println("* To Deposit on an existing account press 2 ");
         System.out.println("* To withdraw from an existing account press 3 ");
         System.out.println("* To transfer form one account to another account press 4");
         System.out.println("* To shutdown the System press 5 ");
